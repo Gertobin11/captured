d06f71fb6c8eb8386c832bfe828e2b642adc0d22
Gertobin11 <gertobin11@gmail.com>
2022-05-25 11:49:51 +0000

    Added functionality to the post create form

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
@@ -9,28 +9,64 @@ import Container from "react-bootstrap/Container";
 import Upload from "../../assets/upload.png";
 
 import styles from "../../styles/PostCreateEditForm.module.css";
-import appStyles from "../../App.module.css";
+import appStyles from '../../App.module.css';
 import btnStyles from "../../styles/Button.module.css";
 
 import Asset from "../../components/Asset";
+import { Figure, Image } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
 
 function PostCreateForm() {
   const [errors, setErrors] = useState({});
 
   const [postData, setPostData] = useState({
-      title: '',
-      content: '',
-      image: ''
-  })
+    title: "",
+    content: "",
+    image: "",
+  });
+
+  const { title, content, image } = postData;
+
+  const imageInput = useRef(null)
+
+  const history = useHistory();
 
   const handleChange = (event) => {
     setPostData({
-        ...postData,
-        [event.target.name]: event.target.value
-    })
-  }
+      ...postData,
+      [event.target.name]: event.target.value,
+    });
+  };
 
-  const {title, content, image} = postData;
+  const handleChangeImage = (event) => {
+    if (event.target.files.length) {
+      URL.revokeObjectURL(image);
+      setPostData({
+        ...postData,
+        image: URL.createObjectURL(event.target.files[0]),
+      });
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append('title', title);
+    formData.append('content', content);
+    formData.append('image', imageInput.current.files[0]);
+
+    try {
+      const {data} = await axiosReq.post('/posts/', formData)
+      history.push(`/posts/${data.id}`)
+    }
+    catch(err) {
+      console.log(err.response?.data);
+      if (err.response?.status !== 401) {
+        setErrors(err.response?.data)
+      }
+    }
+  }
 
   const textFields = (
     <div className="text-center">
@@ -67,19 +103,44 @@ function PostCreateForm() {
   );
 
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Row className={styles.height}>
         <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
           <Container
             className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
           >
             <Form.Group className="text-center">
-              <Form.Label
-                className="d-flex justify-content-center"
-                htmlFor="image-upload"
-              >
-                <Asset src={Upload} message="Click or tap to upload an image" />
-              </Form.Label>
+              {image ? (
+                <>
+                  <Figure>
+                    <Image className={appStyles.Image} src={image} />}
+                  </Figure>
+                  <div>
+                    <Form.Label
+                      className={`${btnStyles.Button} ${btnStyles.Blue} btn`}
+                      htmlFor="image-upload"
+                    >
+                      Change the image
+                    </Form.Label>
+                  </div>
+                </>
+              ) : (
+                <Form.Label
+                  className="d-flex justify-content-center"
+                  htmlFor="image-upload"
+                >
+                  <Asset
+                    src={Upload}
+                    message="Click or tap to upload an image"
+                  />
+                </Form.Label>
+              )}
+              <Form.File
+                onChange={handleChangeImage}
+                id="image-upload"
+                accept="image/*"
+                ref={imageInput}
+              />
             </Form.Group>
             <div className="d-md-none">{textFields}</div>
           </Container>
