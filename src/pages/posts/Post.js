2ebcfc2989184606406e52638aa550680f423fe2
Gertobin11 <gertobin11@gmail.com>
2022-06-01 14:26:14 +0000

    Completed the post detail view page with like / unlike functionality

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Card, Media, OverlayTrigger, Tooltip } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import { axiosRes } from '../../api/axiosDefaults'
 import Avatar from '../../components/Avatar'
 import { useCurrentUser } from '../../contexts/CurrentUserContext'
 
@@ -12,65 +13,106 @@ const Post = (props) => {
         owner,
         profile_id,
         profile_image,
-        comments_count,
-        likes_count,
+        comment_count,
+        like_count,
         like_id,
         title,
         content,
         image,
         updated_pic,
         updated_at,
-        postPage
+        postPage,
+        setPosts
     } = props
 
     const currentUser = useCurrentUser()
     const is_owner = currentUser?.username === owner
-    console.log(is_owner)
-    console.log(currentUser)
+    
+    const handleLike = async () => {
+        try {
+            const { data } = await axiosRes.post("/likes/", { post: id });
+            setPosts((prevPosts) => ({
+                ...prevPosts, results:prevPosts.results.map((post) =>{
+                    return post.id === id
+                    ? {...post, like_count: post.like_count + 1 , like_id: data.id}
+                    : post;
+                })
+            }))
+        }
+        catch(error) {
+            console.log(error)
+        }
+    }
+
+    const handleUnlike = async () => {
+        try {
+            await axiosRes.delete(`/likes/${like_id}`)
+            setPosts((prevPosts) => ({
+                ...prevPosts, results:prevPosts.results.map((post) => {
+                    return post.id === id
+                    ? {...post, like_count: post.like_count -1, like_id: null}
+                    : post;
+                })
+            }))
+        }
+        catch(error) {
+            console.log(error)
+        }
+    }
 
   return (
     <Card className={styles.Post}>
-        <Card.Body>
-            <Media className="align-items-center justify-content-between">
-                <Link to={`/profiles/${profile_id}`} >
-                    <Avatar src={profile_image} height={55} />
-                    {owner}
-                </Link>
-                <div className="d-flex align-items-center">
-                    <span className={styles.date}>{updated_at}</span>
-                    {is_owner && postPage && "..."}
-                </div>
-            </Media>
-        </Card.Body>
-        <Link to={`/posts/$id`}>
-            <Card.Img src={image} alt={title} />
-        </Link>
-        <Card.Body>
-            {title && <Card.Title className="text-center">{title}</Card.Title>}
-            {content && <Card.Text>{content}</Card.Text>}
-            <div className={styles.PostBar}>
-                {is_owner ? (
-                    <OverlayTrigger placement='top' overlay={<Tooltip>You can't like your own post!</Tooltip>}>
-                        <i className='far fa-heart' />
-                    </OverlayTrigger>
-                ) : like_id ? (
-                    <span onClick={() => {}}>
-                        <i className={`fas fa-heart ${styles.Heart}`} />
-                    </span>
-                ) : currentUser ? (
-                    <span onClick={() =>  {}}>
-                        <i className={`far fa-heart ${styles.HeartOutline}`} />
-                    </span>
-                ) : (<OverlayTrigger placement='top' overlay={<Tooltip>You must be logged in to like a post</Tooltip>}>
-                        <i className='far fa-heart' />
-                    </OverlayTrigger>
-                )}
-                {likes_count}
-                {comments_count}
-            </div>
-        </Card.Body>
+      <Card.Body>
+        <Media className="align-items-center justify-content-between">
+          <Link to={`/profiles/${profile_id}`}>
+            <Avatar src={profile_image} height={55} />
+            {owner}
+          </Link>
+          <div className="d-flex align-items-center">
+            <span className={styles.date}>{updated_at}</span>
+            {is_owner && postPage && "..."}
+          </div>
+        </Media>
+      </Card.Body>
+      <Link to={`/posts/$id`}>
+        <Card.Img src={image} alt={title} />
+      </Link>
+      <Card.Body>
+        {title && <Card.Title className="text-center">{title}</Card.Title>}
+        {content && <Card.Text>{content}</Card.Text>}
+        <div className={styles.PostBar}>
+          {is_owner ? (
+            <OverlayTrigger
+              placement="top"
+              overlay={<Tooltip>You can't like your own post!</Tooltip>}
+            >
+              <i className="far fa-heart" />
+            </OverlayTrigger>
+          ) : like_id ? (
+            <span onClick={handleUnlike}>
+              <i className={`fas fa-heart ${styles.Heart}`} />
+            </span>
+          ) : currentUser ? (
+            <span onClick={handleLike}>
+              <i className={`far fa-heart ${styles.HeartOutline}`} />
+            </span>
+          ) : (
+            <OverlayTrigger
+              placement="top"
+              overlay={<Tooltip>You must be logged in to like a post</Tooltip>}
+            >
+              <i className="far fa-heart" />
+            </OverlayTrigger>
+          )}
+          {like_count}
+          <Link to={`/posts/${id}`}>
+            <i className="far fa-comments" />
+          </Link>
+          {comment_count}
+        </div>
+      </Card.Body>
     </Card>
-  )
+  );
 }
 
 export default Post
