5b9835add26aeed8b916d4e9c0dc72d55b4bc9ee
Gertobin11 <gertobin11@gmail.com>
2022-07-12 14:57:24 +0000

    Add and Edit funtctionality added to the comments

--- src/components/MoreDropdown.js
+++ src/components/MoreDropdown.js
@@ -17,7 +17,7 @@ const ThreeDots = React.forwardRef(({ onClick }, ref) => (
 
 export const MoreDropdown = ({ handleEdit, handleDelete }) => {
   return (
-    <Dropdown className="ml-auto" drop="left">
+    <Dropdown className="ml-auto px-2" drop="left">
       <Dropdown.Toggle as={ThreeDots} />
       <Dropdown.Menu
         popperConfig={{ strategy: "fixed" }}
--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -8,33 +8,78 @@ import appStyles from "../../App.module.css";
 import { useParams } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
 
+import { fetchMoreData } from "../../utils/utils";
+import Asset from "../../components/Asset";
 import Post from "./Post";
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import Comment from "../comments/Comment";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function PostPage() {
-  const {id} = useParams();
-  const [post, setPost] = useState({results: []})
+  const { id } = useParams();
+  const [post, setPost] = useState({ results: [] });
 
-    useEffect(() => {
-        const handleMount = async () => {
-            try {
-                const[{data: post}] = await Promise.all([
-                    axiosReq.get(`/posts/${id}`)
-                ])
-                setPost({results: [post]})
-                console.log(post)
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        handleMount()
-    }, [id])
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: post }, { data: comments }] = await Promise.all([
+          axiosReq.get(`/posts/${id}`),
+          axiosReq.get(`/comment/?post=${id}`),
+        ]);
+        setPost({ results: [post] });
+        setComments(comments);
+        console.log(comments.results);
+        console.log(comments.results.length);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    handleMount();
+  }, [id]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
-        <Post {...post.results[0]}  setPosts={setPost} postPage />
-        <Container className={appStyles.Content}>Comments</Container>
+        <Post {...post.results[0]} setPosts={setPost} postPage />
+        <Container className={appStyles.Content}>
+          {currentUser ? (
+            <CommentCreateForm
+              profile_id={currentUser.profile_id}
+              profileImage={profile_image}
+              post={id}
+              setPost={setPost}
+              setComments={setComments}
+            />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
+          {comments.results.length ? (
+            <InfiniteScroll
+              children={comments.results.map((comment) => (
+                <Comment
+                  key={comment.id}
+                  {...comment}
+                  setPost={setPost}
+                  setComments={setComments}
+                />
+              ))}
+              dataLength={comments.results.length}
+              loader={<Asset spinner />}
+              hasMore={!!comments.next}
+              next={() => fetchMoreData(comments, setComments)}
+            />
+          ) : currentUser ? (
+            "No comments yet, be the first to comment!"
+          ) : (
+            "No comments yet"
+          )}
+        </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
         Popular profiles for desktop
